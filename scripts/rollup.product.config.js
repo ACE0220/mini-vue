6655920a80af9c42f37fbaa87ff348ad47ef69a9
ACE0220 <895047954@qq.com>
2022-08-03 10:18:28 +0800

    feat: 环境搭建

--- scripts/rollup.product.config.js
+++ scripts/rollup.product.config.js
@@ -1,30 +1,17 @@
 const path = require('path');
-const babel = require('rollup-plugin-babel');
-const {nodeResolve} = require('@rollup/plugin-node-resolve');
 const pkg = require('../package.json');
-
-const extensions = ['.js'];
+const base = require('./rollup.dev.config');
 
 const resolve = function(...args) {
     return path.resolve(__dirname, ...args);
 }
 
 module.exports = {
-    input: pkg.main,
+    ...base,
     output: {
-        file: resolve('../', pkg.output, "minivue.dev.js"),
-        name: "MiniVue",
+        file: resolve('../', pkg.output, "minivue.product.js"),
+        name: 'MiniVue',
         format: 'umd',
-        sourcemap: true
-    },
-    plugins: [
-        nodeResolve({
-            extensions,
-            modulesOnly: true
-        }),
-        babel({
-            exclude: 'node_modules/**',
-            extensions,
-        }),
-    ]
+        sourcemap: false
+    }
 }
